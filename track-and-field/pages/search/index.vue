53d16f9586589f1d2d1d80ae31a07b30a72e5156
kazwtnb <kazuki.watanabe@bizreach.co.jp>
2022-01-23 17:33:05 +0900

    fix: パンくずリストを一旦削除

--- track-and-field/pages/search/index.vue
+++ track-and-field/pages/search/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="page-content">
-    <v-breadcrumbs :items="breadcrumbs"></v-breadcrumbs>
     <SearchSpike></SearchSpike>
   </div>
 </template>
@@ -12,36 +11,12 @@ import SearchSpike from '~/components/organisms/spike-search/SearchSpike.vue';
 export default defineComponent({
   components: { SearchSpike },
   setup() {
-    return {
-      breadcrumbs: [
-        {
-          text: 'ShoesPicks',
-          href: '/'
-        },
-        {
-          text: 'スパイクを探す',
-          href: '/search'
-        }
-      ]
-    };
+    return {};
   }
 });
 </script>
 <style lang="scss" scoped>
 .page-content {
   padding: 24px 24px 64px;
-
-  h2 {
-    font-size: 20px;
-    + * {
-      margin-top: 24px;
-    }
-  }
-
-  .v-breadcrumbs {
-    + * {
-      margin-top: 8px;
-    }
-  }
 }
 </style>

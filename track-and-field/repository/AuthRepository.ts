e024abdecbdc89665a1ec7d05433065f453fe39e
kazwtnb <kazuki.watanabe@bizreach.co.jp>
2022-03-21 01:44:22 +0900

    WIP: 認証の確認

--- track-and-field/repository/AuthRepository.ts
+++ track-and-field/repository/AuthRepository.ts
@@ -1,15 +1,32 @@
 import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
 // eslint-disable-next-line import/named
 import { ICredentials } from '@aws-amplify/core';
-import { Auth } from 'aws-amplify';
+import { Auth, Hub } from 'aws-amplify';
 
 export class AuthRepository {
-  public loginWithGoogle() {
-    return Auth.federatedSignIn({
+  async loginWithGoogle() {
+    Hub.listen('auth', await this.aa);
+
+    await Auth.federatedSignIn({
       provider: CognitoHostedUIIdentityProvider.Google
     }).then((t: ICredentials) => {
       console.log('ふええ');
       console.log(t);
     });
+  }
+
+  private async aa(data: any) {
+    switch (data.payload.event) {
+      case 'signIn': {
+        // サインインイベントをフック
+        const cognitoUser = await Auth.currentAuthenticatedUser();
+        console.log(cognitoUser);
+        console.log(`signed in ... ${cognitoUser.username}`);
+        Hub.remove('auth', this.aa);
+        break;
+      }
+      default:
+        break;
+    }
   }
 }

942762520dd8590e3c8ab362652e1548d5dabb40
kazwtnb <kazuki.watanabe@bizreach.co.jp>
2022-03-21 14:30:20 +0900

    WIP: 認証の確認

--- track-and-field/plugins/amplify.ts
+++ track-and-field/plugins/amplify.ts
@@ -1,23 +1,24 @@
 import { Amplify } from 'aws-amplify';
 import awsConfig from '../aws-exports';
 
-const isLocalhost: boolean = process.env.ENV === 'local';
-
-const [redirectSignIn, localRedirectSignIn] =
-  awsConfig.oauth.redirectSignIn.split(',');
-
-const [redirectSignOut, localRedirectSignOut] =
-  awsConfig.oauth.redirectSignOut.split(',');
-
-const updatedAwsConfig = {
-  ...awsConfig,
-  oauth: {
-    ...awsConfig.oauth,
-    redirectSignIn: !isLocalhost ? redirectSignIn : localRedirectSignIn,
-    redirectSignOut: !isLocalhost ? redirectSignOut : localRedirectSignOut
-  }
-};
+// const isLocalhost: boolean = process.env.ENV === 'local';
+//
+// const [redirectSignIn, localRedirectSignIn] =
+//   awsConfig.oauth.redirectSignIn.split(',');
+//
+// const [redirectSignOut, localRedirectSignOut] =
+//   awsConfig.oauth.redirectSignOut.split(',');
+//
+// const updatedAwsConfig = {
+//   ...awsConfig,
+//   oauth: {
+//     ...awsConfig.oauth,
+//     redirectSignIn: !isLocalhost ? redirectSignIn : localRedirectSignIn,
+//     redirectSignOut: !isLocalhost ? redirectSignOut : localRedirectSignOut
+//   }
+// };
 
 export default () => {
-  Amplify.configure(updatedAwsConfig);
+  // Amplify.configure(updatedAwsConfig);
+  Amplify.configure(awsConfig);
 };

bae7930ffbf12990f8027cf5bbba9d6ce6f37f95
akazupin <akazupinsan@gmail.com>
2022-09-30 13:22:05 +0900

    fomat: ES LintとPrettierでフォーマッタかけてみた

--- picker-app/src/pages/_app.tsx
+++ picker-app/src/pages/_app.tsx
@@ -1,22 +1,24 @@
 import 'styles/globals.css';
+import { ReactElement, ReactNode } from 'react';
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
-import { ReactElement, ReactNode } from 'react';
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+export type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
 
 const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
-  const getLayout = Component.getLayout || ((page) => page)
+  const getLayout = Component.getLayout || (page => page);
 
-  return getLayout(<>
-    <Component {...pageProps} />
-  </>)
-}
+  return getLayout(
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-export default MyApp
+export default MyApp;
--- picker-app/src/pages/index.tsx
+++ picker-app/src/pages/index.tsx
@@ -1,22 +1,17 @@
+import { ReactElement } from 'react';
 import DefaultLayout from 'layout/default';
 import { NextPageWithLayout } from 'pages/_app';
-import { ReactElement } from 'react';
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
       <p>index</p>
     </>
-  )
-}
+  );
+};
 
 Home.getLayout = (page: ReactElement) => {
-  return (
-    <DefaultLayout>
-      {page}
-    </DefaultLayout>
-  )
-}
+  return <DefaultLayout>{page}</DefaultLayout>;
+};
 
-
-export default Home
+export default Home;

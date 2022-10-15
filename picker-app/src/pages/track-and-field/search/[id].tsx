0fd4c9bbbc4b527f4cdc616b29eea30aad498cc5
kazwtnb <kazuki.watanabe@bizreach.co.jp>
2022-10-15 14:47:38 +0900

    fix: pageコンポーネントのgetStaticProps/ getStaticPathsではバックエンドのコードをそのまま呼ぶように修正

--- picker-app/src/pages/track-and-field/search/[id].tsx
+++ picker-app/src/pages/track-and-field/search/[id].tsx
@@ -2,18 +2,10 @@ import { ReactElement } from 'react';
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext, InferGetStaticPropsType } from 'next';
 import { TAF_SEARCH_PAGE_PATH } from 'features/track-and-field/constants/routing-path';
 import { TAFSpikeTemplate } from 'features/track-and-field/spike/TAFSpikeTemplate';
-import { useFetchData } from 'graphql/custom-fetcher';
-import {
-  Spike,
-  SpikeDocument,
-  SpikeQuery,
-  SpikeQueryVariables,
-  SpikesDocument,
-  SpikesQuery,
-  SpikesQueryVariables,
-} from 'graphql/generated/codegen-client';
+import { Spike } from 'graphql/generated/codegen-client';
 import TAFLayout from 'layout/TrackAndField';
 import { NextPageWithLayout } from 'pages/_app';
+import { spikeLoader, spikesLoader } from 'server/loader/track-and-field/spikeLoader';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps> & {
   spike: Spike;
@@ -36,26 +28,23 @@ export const getStaticProps: GetStaticProps = async ({ params }: GetStaticPropsC
     throw 'idが存在しません';
   }
 
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const data = await useFetchData<SpikeQuery, SpikeQueryVariables>(SpikeDocument).bind(null, {
-    input: { id: params.id.toString() ?? '' },
-  })();
+  // getStaticPropsはバックエンドで動作するので、バックエンドのコードをそのまま呼ぶ
+  const data = await spikeLoader({ id: params.id.toString() });
 
   console.log('getStaticProps');
-  console.log(data);
+  console.log(data[0]);
 
   return {
     props: {
-      spike: data.spike[0],
+      spike: data[0],
     },
   };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const data = await useFetchData<SpikesQuery, SpikesQueryVariables>(SpikesDocument).bind(null, { input: {} })();
-
-  const paths = data?.spikes.map(spike => `${TAF_SEARCH_PAGE_PATH}/${spike.id}`) ?? [];
+  // getStaticPathsはバックエンドで動作するので、バックエンドのコードをそのまま呼ぶ
+  const data = await spikesLoader({});
+  const paths = data?.map(spike => `${TAF_SEARCH_PAGE_PATH}/${spike.id}`) ?? [];
 
   return {
     paths: paths,
